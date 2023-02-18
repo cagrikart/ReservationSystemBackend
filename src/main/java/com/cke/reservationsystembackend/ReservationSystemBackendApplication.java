633efe9e1dc5b5dfeea7d71744a169b42783d0e5
Çağrı KART <ckarte43@gmail.com>
2023-02-19 00:29:03 +0300

    Initial commit

---
+++ src/main/java/com/cke/reservationsystembackend/ReservationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cke.reservationsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cke/reservationsystembackend/ReservationSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cke.reservationsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
